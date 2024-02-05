48640ab8ccce725425df57041eae60bc0fb0e996
Veirt <veirt@duck.com>
2024-02-05 22:45:53 +0800

    feat: ask about image

--- src/the_fool/cogs/bard.py
+++ src/the_fool/cogs/bard.py
@@ -1,3 +1,5 @@
+import aiohttp
+import logging
 from discord.ext import commands
 from ..bard import bard_instance
 from ..config import DISCORD_BARD_CHANNEL_ID
@@ -8,42 +10,43 @@ async def setup(bot):
     await bot.add_cog(Bard(bot))
 
 
+async def get_image(image_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(image_url) as resp:
+            print(resp.status)
+            return await resp.read()
+
+
 class Bard(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author.bot or message.content.startswith(self.bot.command_prefix):
-            return
+    async def _get_bard_answer(self, ctx, question):
+        if len(ctx.message.attachments) > 1:
+            ctx.send(
+                "Cannot read more than one image at a time. If you send multiple images, only the first one will be read."
+            )
+        attachments = list(
+            filter(
+                lambda attachment: attachment.filename.endswith(".png")
+                or attachment.filename.endswith(".jpg"),
+                ctx.message.attachments,
+            )
+        )
 
-        if message.channel.id == DISCORD_BARD_CHANNEL_ID:
-            ctx = await self.bot.get_context(message)
-            async with ctx.typing():
-                try:
-                    response = bard_instance.get_answer(message.content)
-                    links = response["links"]
-                    answer = response["content"]
-                except Exception as e:
-                    await message.channel.send(f"An error happened. Error: {e}")
-                    return
-
-            if len(answer) > MAX_MESSAGE_LEN:
-                msg_chunks = split_message_to_chunks(answer)
-                for msg in msg_chunks:
-                    await message.channel.send(msg)
-
-                return
-
-            await message.channel.send(answer)
-            if len(links) > 0:
-                await message.channel.send("\n".join(links))
-
-    @commands.command()
-    async def bard(self, ctx, *args):
         async with ctx.typing():
             try:
-                answer = bard_instance.get_answer("".join(args))["content"]
+                if len(attachments) > 0:
+                    image = await get_image(attachments[0].url)
+                    print(image)
+                    response = bard_instance.ask_about_image(question, image)
+                else:
+                    logging.debug("Bard: Regular question")
+                    print(attachments, ctx.message.attachments)
+                    response = bard_instance.get_answer(question)
+
+                links = response["links"]
+                answer = response["content"]
             except Exception as e:
                 await ctx.send(f"An error happened. Error: {e}")
                 return
@@ -56,3 +59,23 @@ class Bard(commands.Cog):
             return
 
         await ctx.send(answer)
+        if len(links) > 0:
+            await ctx.send("\n".join(links))
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if (
+            message.author.bot  # if author is a bot
+            or message.content.startswith(
+                self.bot.command_prefix
+            )  # if it starts with the prefix
+            or message.channel.id != DISCORD_BARD_CHANNEL_ID  # if it's not
+        ):
+            return
+
+        ctx = await self.bot.get_context(message)
+        await self._get_bard_answer(ctx, message.content)
+
+    @commands.command()
+    async def bard(self, ctx, *args):
+        await self._get_bard_answer(ctx, "".join(args))
